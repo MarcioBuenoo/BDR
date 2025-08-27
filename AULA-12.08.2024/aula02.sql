60c7ca8b9ad10f6cd845b66186fd1b00aa7c2ce3
Márcio Roberto Bueno Júnior <marcinbueno01@hotmail.com>
2025-08-26 21:49:18 -0300

    Add files via upload

---
+++ AULA-12.08.2024/aula02.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE queimadas_db;
+
+CREATE TABLE focos_calor (
+	id SERIAL PRIMARY KEY,
+	estado VARCHAR(50),
+	bioma VARCHAR(50),
+	data_ocorrencia DATE
+);
+
+INSERT INTO focos_calor (estado, bioma, data_ocorrencia)
+VALUES ('São Paulo', 'Mata Atlântica', '2025-02-10');
+
+SELECT * FROM focos_calor;
